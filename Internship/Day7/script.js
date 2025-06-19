418331c4641cb3bd9adb1a417db810a84839e963
Himangshu <himangshukumardas75695@gmail.com>
2025-06-19 10:15:56 +0530

    new changes has been added

---
+++ Internship/Day7/script.js
@@ -0,0 +1,31 @@
+let a = 10;
+let b = 5;
+
+// if(a==b){
+//     console.log(true);
+
+// }else{
+//     console.log(false);
+// }// == only check value  
+
+// if(a==b && b==a){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// if(a===b  ||b==a){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// if((a===b||b==a)||(a===b&&b==a)){
+//     console.log(false);
+// }else{
+//     console.log(true);
+// }
+
+//ternary operartor
+let c = a>b?"a is greater than b ":"b is greater than a ";
+console.log(c);
