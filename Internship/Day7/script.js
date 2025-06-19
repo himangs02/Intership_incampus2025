f976c5a0c70d14e57df01a9f4192b190ffc13f4f
Himangshu <himangshukumardas75695@gmail.com>
2025-06-19 10:44:07 +0530

    functions notes commited

--- Internship/Day7/script.js
+++ Internship/Day7/script.js
@@ -1,5 +1,5 @@
-let a = 10;
-let b = 5;
+// let a = 10;
+// let b = 5;
 
 // if(a==b){
 //     console.log(true);
@@ -31,3 +31,48 @@ let b = 5;
 let c = a>b?"a is greater than b ":"b is greater than a ";
 console.log(c);
 */
+
+//loops
+//FOR OF LOOP
+// let arr = [1,2,3,4,5,6,7,8,9,10];
+
+// let sum = 0 ;
+// for (let element of arr) {
+//     sum +=element;
+    
+    
+// }
+// console.log(sum);'
+
+
+//FOR IN LOOP 
+// let person = {
+//     name : "Himangshu",
+//     age : 18,
+//     gender : "male"
+// }
+// for (let key in person) {
+//     console.log(key,person[key]);
+
+// }
+
+
+// let arr = [1,2,3,4,5,6,7,8,9,10];
+
+// for (let key in arr){
+//     console.log(key,"=>",arr[key]);
+// }
+
+//FUNCTION
+
+// functions it is a set of instruction that can be used repeatedly
+
+// let a = 10;
+// let b = 5;
+// function add(){
+//     return a+b;
+// }
+
+// console.log(add());
+
+
