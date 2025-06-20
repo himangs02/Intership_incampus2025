eea1358efbd96f537da9e63acab827f19a755d69
Himangshu <himangshukumardas75695@gmail.com>
2025-06-20 09:56:00 +0530

    rest of the code commited

--- Internship/Day7/script.js
+++ Internship/Day7/script.js
@@ -80,7 +80,7 @@ console.log(c);
 // arror function
 
 let score =0;
-let Timer=60;
+// let Timer=60;
 
 
 let Random=()=>{
@@ -111,16 +111,16 @@ btn.addEventListener("click",()=>{
     bj.style.backgroundColor=Random();
 })
 
-let timer=document.getElementById("timer");
-setInterval(()=>{
-    if(Timer>0){
-    timer.innerText=Timer--;
-    }else if(Timer==0){
-        timer.innerText="Time is Over";
-        timer.style.color="red";
-        Timer=60;
-    }
-},1000);
+// let timer=document.getElementById("timer");
+// setInterval(()=>{
+//     if(Timer>0){
+//     timer.innerText=Timer--;
+//     }else if(Timer==0){
+//         timer.innerText="Time is Over";
+//         timer.style.color="red";
+//         Timer=60;
+//     }
+// },1000);
 
 
 ///--------------------------------------------------------------------------
@@ -156,3 +156,67 @@ setInterval(()=>{
 // }
 // let sqr=arr.map(square);
 // console.log(sqr);
+
+// function cb(x) {
+//     console.log("This is a callback function");
+//     return function (y) {
+//         console.log("This is an inner function");
+//         return x + y;
+//     };
+// }
+
+// function higher(callback) {
+//     console.log("This is a higher order function");
+//     let innerfunc = callback(5); // call the callback with argument 5
+//     console.log("This is below callback function");
+//     return innerfunc(10); // call the returned inner function with 10
+// }
+
+// console.log("Final result:", higher(cb));
+
+
+
+
+// let num = prompt("Enter a number");
+
+// if(num==0){
+//     console.log("Number is zero");
+// }
+// else if(num>0){
+//     console.log("Number is greater than zero");
+// }else{
+//     console.log("Number is less than zero");
+// }
+
+
+// let num2=num==0?"Number is zero":num>0?"Number is greater than zero":"Number is less than zero";
+// console.log(num2);
+// if (isNaN(num)) {
+//     console.log("Number is not a number");
+// } else if (num ==0) {
+//     console.log("Number is zero");
+// } else if (num > 0) {
+//     console.log("Number is greater than zero");
+// } else {
+//     console.log("Number is less than zero");
+// }
+
+// let num2=num!==num?"Number is not a number":num==0?"Number is zero":num>0?"Number is greater than zero":"Number is less than zero";
+// console.log(num2);
+
+
+//-----------------------------------------
+//Cube Arr
+// let arr=[1,2,3,4,5,6,7,8,9];
+// cubArr=arr.map((num)=>{
+//     return num*num*num;
+
+// });
+
+// console.log(cubArr);
+
+let arr3=[1,2,3,4,5,6,7,8,9];
+// oddNum=arr3.filter((num)=>{
+//     return num%2!=0;
+// })
+;
